f36aabed1bd154807a3cfad46962b386eb380be7
murshidp <murshidparambadan@gmail.com>
2020-05-01 20:24:32 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      todoList:[],
+      activeItem:{
+        id:null,
+        title:"",
+        completed:false
+      },
+      editing:false
+    }
+    this.fetchTasks=this.fetchTasks.bind(this)
+  };
+  componentWillMount(){
+    this.fetchTasks()
+  }
+  fetchTasks(){
+    fetch('http://127.0.0.1:8000/api/task-list/')
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+          <div id="form">
+              <div className="flex-wrapper">
+                <div style={{flex:6}}>
+                  <input className="form-control" id="title" name="title" type="text" placeholder="Add Task" />
+                </div>
+                <div style={{flex:1}}>
+                  <input className="btn btn-warning" id="submit" name="Add" type="submit"  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="list-wrapper">
+           
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default App;
