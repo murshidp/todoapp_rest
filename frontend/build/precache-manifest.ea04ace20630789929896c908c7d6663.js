8e8fddd8345c2f0202fdc4f9348fbe62952bdee8
murshidp <murshidparambadan@gmail.com>
2020-05-02 07:53:58 +0530

    buid react app

---
+++ frontend/build/precache-manifest.ea04ace20630789929896c908c7d6663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c7627afdf7a68c4cf3d18ecbfcff0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea19bc182f9bb8e86b8",
+    "url": "/static/css/main.329fcd1d.chunk.css"
+  },
+  {
+    "revision": "03d882d5612fd560d7ae",
+    "url": "/static/js/2.15316b9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15316b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea19bc182f9bb8e86b8",
+    "url": "/static/js/main.11f0df0a.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
